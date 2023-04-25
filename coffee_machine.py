db7ba93caf6bbc0be9ab736a7eef3bc62fa1471a
henri201 <64684431+henri201@users.noreply.github.com>
2023-04-25 23:06:05 +0300

    Update coffee_machine.py

--- coffee_machine.py
+++ coffee_machine.py
@@ -7,3 +7,19 @@ while is_on:
     choice = input("what would you like? (espresso/latte/cappuchino): ")
     if choice == "off":
         is_on = False
+        
+        
+        
+        Print report.
+a. When the user enters “report” to the prompt, a report should be generated that shows
+the current resource values. e.g.
+Water: 100ml
+Milk: 50ml
+Coffee: 76g
+Money: $2.5
+4. Check resources sufficient?
+a. When the user chooses a drink, the program should check if there are enough
+resources to make that drink.
+b. E.g. if Latte requires 200ml water but there is only 100ml left in the machine. It should
+not continue to make the drink but print: “Sorry there is not enough water.”
+c. The same should happen if another resource is depleted, e.g. milk or coffee.
