662264d126bbfe1cc56d7ad5ced8c6e31f576c71
hpriim2gi <henri.priim2gi@hotmail.com>
2023-04-26 22:49:03 +0300

    first commit

--- coffee_machine.py
+++ coffee_machine.py
@@ -1,9 +1,25 @@
 from menu import MENU, resources
 import os
 
+
+
+profit = 0
+def is_resource_sufficient(order_ingredience):
+    
+
+
 is_on = True
 
 while is_on:
+    current_resources = resources
     choice = input("what would you like? (espresso/latte/cappuchino): ")
     if choice == "off":
         is_on = False
+    elif choice == "report":
+        print(f"water: {resources['water']}ml")
+        print(f"milk: {resources['milk']}ml")
+        print(f"coffee: {resources['coffee']}ml")
+        print(f"Money: ${profit}")
+    else:
+        drink = MENU[choice]
+        is_resource_sufficient(drink["ingredience"])
