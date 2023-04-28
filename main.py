d7ccf6662aacead75d832985ae97cc53ff4368ad
henri201 <64684431+henri201@users.noreply.github.com>
2023-04-28 22:50:30 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from menu import Menu
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+money_machine.report()
+coffee_maker.report()
